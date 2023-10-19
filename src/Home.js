38439a7ce1eadb8bcb9dcc43367ab552e2739b9d
d3ku45 <53232960+d3ku45@users.noreply.github.com>
2023-10-19 22:03:07 +0530

    Update app design

---
+++ src/Home.js
@@ -0,0 +1,123 @@
+import React from "react";
+import img1 from './images/image 1.jpg';
+import { useNavigate } from "react-router-dom";
+
+
+
+function Home()
+{
+    const navigate=useNavigate();
+    function handleClick()
+    {
+        navigate("/Login");
+    }
+
+    const navigate1=useNavigate();
+    function handleClick1()
+    {
+        navigate1("/Signup");
+    }
+
+    return(
+        <div className="Home bg-[#EEFFF0]">
+           <div className="py-5 px-20 ">
+                <nav class="bg-black p-4 rounded-xl h-20">
+                    <div class="container mx-auto flex">
+                        <div class="text-[#ADFF45] text-4xl font-extrabold px-6 ml-28 mr-10">URBAN LINK</div>
+                        <div>
+                            <ul class="flex space-x-6 py-2.5 px-6 text-lg">
+                                <li><a href="#" class="text-white hover:text-gray-300">Home</a></li>
+                                <li><a href="#" class="text-white hover:text-gray-300">About</a></li>
+                                <li><a href="#" class="text-white hover:text-gray-300">Contact</a></li>
+                            </ul>
+                        </div>
+
+                        <div className="lgbutton flex ml-auto mr-10 space-x-5">
+                            <button onClick={handleClick} className="bg-[#ADFF45] px-8 rounded-xl text-lg hover:bg-[#aeff45ed]">
+                                Login
+                            </button>
+
+                            <button onClick={handleClick1} className="bg-[#EEFFF0] px-8 rounded-xl text-lg hover:bg-[#ADFF45]">
+                                Signup
+                            </button>
+
+                        </div>
+                    </div>
+                </nav>
+            </div> 
+
+
+            <div className="flex">
+                <div className="w-1/2">
+                    <div className="mx-28 text-6xl mt-48 font-semibold font-inter">
+                    RIDE TOGETHER,
+                    SAVE TOGETHER.
+                    </div>
+
+                    <div className="mx-28 text-2xl font-inter font-semibold my-5">
+                    Shared Rides, Split Costs: Your Pathway to Affordable Travel!
+                    </div>
+                </div>
+
+                
+                
+
+                <div className="w-1/2">
+                    <div className="mt-28 mr-16">
+                        <img src={img1} alt=""></img>
+                        
+                    </div>
+                        
+                </div>
+            </div>
+
+
+            <div className="flex mt-40">
+                <div className="w-1/2 ml-28 mb-20">
+                    <h className="text-6xl font-inter font-semibold">RIDE SEARCH</h>
+
+                    <div class="flex flex-col gap-6 max-w-sm ml-3 p-4 bg-[#EEFFF0] mt-12 rounded-xl ">
+                        
+                        <input 
+                            type="text"
+                            placeholder="From"
+                            className="py-2 px-5 border bg-[#ADFF45] border-gray-300 rounded-xl placeholder:text-black"
+                        />
+                        <input
+                            type="text"
+                            placeholder="To"
+                            className="py-2 px-5 border bg-[#ADFF45] border-gray-300 rounded-xl placeholder:text-black "
+                        />
+                        <input
+                            type="Date"
+                            className="py-2 px-5 border bg-[#ADFF45] border-gray-300 rounded-xl"
+                        />
+                        <input
+                            type="Time"
+                            className="py-2 px-5 border bg-[#ADFF45] border-gray-300 rounded-xl"
+                        />
+                        <button
+                            className="bg-black hover:bg-gray-800 text-white font-bold py-2 rounded-xl"
+                        >
+                            Search
+                        </button>
+                    </div>
+                    
+            
+                </div>
+
+
+                <div className="w-1/2 mx-auto my-40">
+                    <p className="text-3xl mr-44 font-semibold font-inter text-center">Your Ride Is Just A Search Away! Enter Your Destination To Find Available Rides And Start Your Journey Hassle Free</p>
+
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Home;
+
+
+
---
+++ src/Login.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {Link} from "react-router-dom";
+import fuser from './images/fusername.jpg';
+import fpass from './images/Fpass.jpg';
+
+
+function Login()
+{   
+
+
+    return(
+
+
+        
+                <div className="bg-black">
+                    <div className="flex">
+                        <div className="left w-7/12 bg-black text-[#ADFF45] px-10 py-8 h-screen flex flex-col items-center justify-center">
+
+                            <Link to="/" className='font-extrabold mb-6 text-8xl mx-auto'>
+                                URBAN LINK
+                            </Link>
+
+                            <div className="flex mx-auto text-white">
+                                        <p className="mt-6 text-xl">Don’t have an account ?</p>
+                                        
+                                        <Link to="/Signup" className='font-extrabold mt-6 text-xl ml-2 hover:text-[#ADFF45]'>
+                                                Signup
+                                        </Link>
+
+                            </div>
+                            
+                           
+                          
+
+                        </div>
+                    
+                        <div className="right w-1/2 bg-[#CAFF87] px-10 py-8 h-screen rounded-l-3xl">
+                            <h1 className="font-semibold mb-4 text-4xl pt-16 pb-6 font-inter mx-60">Sign In</h1>
+                            <form action="#">
+                                <div className="inputbox flex flex-col mx-28">
+                                    
+                                    
+                                    <div className="relative flex">
+                                        <span><img src={fuser} className="w-6 h-6 absolute ml-4 mt-2"></img></span>
+                                        <input type="text" placeholder="Email or Username" className="px-12 py-2 rounded-md mb-9 shadow-xl border border-black w-full" required></input>   
+                                    </div>
+
+                                     
+                                    <div className="relative flex">
+                                        <span><img src={fpass} className="w-6 h-6 absolute ml-4 mt-2"></img></span>
+                                        <input type="password" placeholder="Password" className="px-12 py-2 rounded-md border border-black mb-9 shadow-xl w-full" required></input>  
+                                    </div>
+
+                                    
+                                   
+                                    
+
+                                    <button className="signinbtn font-bold mb-12 py-3 px-4 bg-black text-white text-xl rounded-xl shadow-lg hover:bg-gray-800">LOGIN</button>
+                                    <hr className="border border-black mb-2"></hr>
+
+
+                                    <p className="font-bold text-xl mx-auto">OR</p>
+                                    <button className="flex flex-row text-base rounded-lg bg-[#CAFF87] text-black border border-black px-24 py-3 my-2 font-semibold hover:bg-[#D5FFA1]"><span><img class="h-7 w-7 rounded-full mx-2" src="https://pbs.twimg.com/profile_images/1605297940242669568/q8-vPggS_400x400.jpg" alt="google"></img></span>Continue with Google</button>
+
+                                    
+                                </div>
+                            </form>
+
+
+                        </div>
+
+                        
+
+                        
+                    </div>    
+
+
+                </div>
+        
+    );
+}
+
+export default Login;
