b310568f948384d8b7bec70d57b236b267fef363
Rajath <rajathganeshmt@gmail.com>
2023-10-19 22:09:20 +0530

    Remove unwanted import from Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import {Link} from "react-router-dom";
 import fuser from './images/fusername.jpg';
 import fpass from './images/Fpass.jpg';
-
 
 function Login()
 {   
